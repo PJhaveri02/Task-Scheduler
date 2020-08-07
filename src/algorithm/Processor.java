5a9c11411d9bedb68969fad359ac4dbe334f8e74
Kenny Tang <kennyt12237@gmail.com>
2020-08-07 19:50:30 +1200

    modified Tasks class to include edge weights

--- src/algorithm/Processor.java
+++ src/algorithm/Processor.java
@@ -3,5 +3,10 @@ package algorithm;
 import java.util.ArrayList;
 
 public class Processor {
+
+    private int _id;
     private ArrayList<Tasks> tasks;
+
+
+
 }
--- src/algorithm/Tasks.java
+++ src/algorithm/Tasks.java
@@ -1,12 +1,30 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Tasks {
 
     private int _weight;
     private int _start;
     private Processor _processor;
-    private ArrayList<Tasks> _dependentTasks;
+    private Map<Tasks,Integer> _dependentsAndWeight;
 
+    public Tasks(int weight, int start, Map<Tasks,Integer> dependentsToWeights){
+        _weight = weight;
+        _start = start;
+        _dependentsAndWeight = dependentsToWeights;
+    }
+
+    public int get_weight() {
+        return _weight;
+    }
+
+    public int get_start() {
+        return _start;
+    }
+
+    public Processor get_processor() {
+        return _processor;
+    }
 }
