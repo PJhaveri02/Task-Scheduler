64f0cd8941527ed9b4aec875cc40743a2e708fb3
Kenny Tang <kennyt12237@gmail.com>
2020-08-07 19:59:28 +1200

    Added initializable method

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,12 +1,17 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Main extends Application implements Initializable {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -16,8 +21,18 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    /**
+     * Initial setup of application after FXML objects have been injected
+     * @param url
+     * @param resourceBundle
+     */
+    @FXML
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
