36d14aaad3f0a8ab6f70e383bf8257de83aaf07c
OverCry <wcho400@aucklanduni.ac.nz>
2020-08-20 15:50:41 +1200

    started on the fxml file

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -22,10 +22,10 @@ public class Main extends Application implements Initializable {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
