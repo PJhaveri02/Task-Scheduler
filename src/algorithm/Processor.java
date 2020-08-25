e5da1323e7fe4e241a76fa3ef05cf3afad4a8683
Daniel Yeom <dyeo713@aucklanduni.ac.nz>
2020-08-25 16:01:20 +1200

    finished Processor.clone()

--- src/algorithm/Processor.java
+++ src/algorithm/Processor.java
@@ -50,8 +50,6 @@ public class Processor implements Cloneable {
 //        System.out.println(endTime);
         _time = endTime;
         _endTime.put(node, endTime);
-//        node.setProcessor(this);
-        //node.setStart(startTime);
     }
 
     public int getEnd(Node n){
@@ -81,82 +79,32 @@ public class Processor implements Cloneable {
     }
 
 
-    //unsure about its purpose
-    /**
-     * Essentially a duplicate of the scheduleTask but takes into consideration of switching processors.
-     * Follows the 'Precedence Constraint'
-     * Adds the task to the processor and increment the internal timer of latest task.
-     * Delegates the processor to the node as well.
-     *
-     * @param toAssignNode an available node ready to be assigned
-     * @param prevNode     the previously assigned node
-     */
-//    public void scheduleTaskOnProcessor(Node toAssignNode, Node prevNode) {
-//
-//        // Set the initial start time to the internal processor time
-//        int startTime = 0;
-//
-//        int startDepTime = prevNode.getStart();
-//        int executionDepTime = prevNode.get_weight();
-//        int communicationTime = toAssignNode.getDependentsAndWeight().get(prevNode);
-//
-//        // Set start time of the tasks. Follows the 'Precedence Constraint' here
-//        if (prevNode.getProcessor().equals(this)) {
-//            startTime = _time;
-//        } else {
-//            startTime = startDepTime + executionDepTime + communicationTime;
-//        }
-//
-//        _tasks.add(toAssignNode);
-//        _time += toAssignNode.get_weight();
-//        toAssignNode.setProcessor(this);
-//        toAssignNode.setStart(startTime);
-//    }
-
-
     /**
      * makes a deep copy of a Processor object
      *
      * @return deep copy of Processor
      * @throws CloneNotSupportedException
      */
-//    public Object clone() throws CloneNotSupportedException {
-//        //create a shallow copy
-//        Processor p = (Processor) super.clone();
-//
-//        //make a deep copy by creating new list of tasks from the old list
-//        List<Node> cloneTasks = new ArrayList<Node>();
-//        //make copy of each task, add it to the new list
-//        for (Node task : this._tasks) {
-//            //this method only sets weight, ID, and name fields for the Node object.
-//            //it does NOT (yet) set the other fields in 'Node', such as _bottomWeight, _start, or _processor.
-//            //need _start, _processor, _dependenciesAndWeight, etc for writing the cloned Processors in the output file
-//            Node cloneTask = new Node(task.get_weight(), task.getId(), task.toString());
-//            cloneTasks.add(cloneTask);
-//
-//            // set the tasks starting time and bottom level
-//            cloneTask.setStart(task.getStart());
-//            cloneTask.setBottomLevel(task.getBottomLevel());
-//        }
-//
-//        // Once the task objects are cloned, set the _dependenciesAndWeight field.
-//        // Can only be done once all the tasks are instantiated.
-//        //needs review
-////        for (Node task : _tasks) {
-////            // Get the cloned task corresponding to the task
-////            Node cloneTask = cloneTasks.get(_tasks.indexOf(task));
-////
-////            Map<Node, Integer> dependentsAndWeight = task.getDependentsAndWeight();
-////            for (Node depTask : dependentsAndWeight.keySet()) {
-////                Node depClonedTask = cloneTasks.get(cloneTasks.indexOf(depTask));
-////                cloneTask.addDependency(depClonedTask, dependentsAndWeight.get(depTask));
-////            }
-////        }
-//
-//        //set the cloned task list in the cloned Processor
-//        p._tasks = cloneTasks;
-//        return p;
-//    }
+    public Object clone() throws CloneNotSupportedException {
+        //create a shallow copy
+        Processor p = (Processor) super.clone();
+
+        //generate a list of the currently scheduled nodes
+        List<Node> cloneTasks = new ArrayList<Node>();
+        for (Node n : _tasks){
+            cloneTasks.add(n);
+        }
+
+        //set the cloned tasks into the new processor clone
+        p._tasks = cloneTasks;
+
+        //generate a new map for the end times
+        Map<Node, Integer> cloneET = new HashMap<Node, Integer>();
+        cloneET.putAll(_endTime);
+        p._endTime = cloneET;
+
+        return p;
+    }
 
 
 }
